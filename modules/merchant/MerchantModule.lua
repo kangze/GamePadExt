f3c5d656f74dd24c7744d4d48c18f57379f98c94
kangze <kangze25@hotmail.com>
2023-11-21 23:19:55 +0800

    Revert "1"
    This reverts commit ec54651f934607f8972a83c71f4fbef10ba0dd7d.

--- modules/merchant/MerchantModule.lua
+++ modules/merchant/MerchantModule.lua
@@ -24,6 +24,20 @@ function MerchantModule:OnEnable()
             CreateFrame("Frame", "MerchantItem" .. i, MerchantFrame, "MerchantItemTemplate");
         end
     end
+
+    --创建一个滚动窗体
+    -- Create the scroll frame
+    local scrollFrame = CreateFrame("ScrollFrame", nil, UIParent, "UIPanelScrollFrameTemplate")
+    scrollFrame:SetSize(200, 200)
+    scrollFrame:SetPoint("CENTER")
+
+    -- Create the scroll child
+    local scrollChild = CreateFrame("Frame", nil, scrollFrame)
+    scrollFrame:SetScrollChild(scrollChild)
+
+    -- Set the scroll child's size
+    scrollChild:SetSize(200, 400) -- The height is 2000 to allow for lots of buttons
+    self.scrollChild = scrollChild;
 end
 
 --Sample:Masque
