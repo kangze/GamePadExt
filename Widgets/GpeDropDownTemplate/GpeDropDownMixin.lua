08d039fc1b0b342fb32dfa0d1568c6fbab96ae46
kangze <kangze25@hotmail.com>
2024-03-01 16:15:28 +0800

    add

--- Widgets/GpeDropDownTemplate/GpeDropDownMixin.lua
+++ Widgets/GpeDropDownTemplate/GpeDropDownMixin.lua
@@ -11,6 +11,16 @@ end
 function GpeDropDownMixin:OnLeave()
 end
 
---展开子选项
+--展开子选项面板
 function GpeDropDownMixin:Expand()
+    self.container:Show();
+end
+
+--添加项
+function GpeDropDownMixin:AddItem(item)
+    local width, height = self.container:GetSize();
+    height = height + 20;
+    self.container:SetSize(width, height);
+    local frame = CreateFrame("Frame", nil, self.container, "GpeDropDownItemTemplate");
+    frame.text:SetText(item.text);
 end
