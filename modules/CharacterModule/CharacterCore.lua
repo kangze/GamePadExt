134ccc12a84e08b32fefbc7b04d85413c57d428f
kangze <kangze25@hotmail.com>
2024-03-09 21:30:59 +0800

    完成属性的赋值

--- modules/CharacterModule/CharacterCore.lua
+++ modules/CharacterModule/CharacterCore.lua
@@ -367,14 +367,160 @@ function GetUserArmor()
         armorReductionAgainstTarget = armorReductionAgainstTarget * 100;
     end
     local tooltip = HIGHLIGHT_FONT_COLOR_CODE ..
-    format(PAPERDOLLFRAME_TOOLTIP_FORMAT, ARMOR) .. " " .. BreakUpLargeNumbers(effectiveArmor) .. FONT_COLOR_CODE_CLOSE;
+        format(PAPERDOLLFRAME_TOOLTIP_FORMAT, ARMOR) ..
+        " " .. BreakUpLargeNumbers(effectiveArmor) .. FONT_COLOR_CODE_CLOSE;
     local tooltip2 = format(STAT_ARMOR_TOOLTIP, armorReduction);
     local tooltip3;
     if (armorReductionAgainstTarget) then
         tooltip3 = format(STAT_ARMOR_TARGET_TOOLTIP, armorReductionAgainstTarget);
     end
     print(tooltip2);
-    
 end
 
 GetUserArmor();
+
+--schools:
+-- 1物理
+-- 2神圣
+-- 3火焰
+-- 4自然
+-- 5冰霜
+-- 6暗影
+-- 7奥术
+
+
+--获取玩家的暴击信息
+function GetBaoJi()
+    local rating;
+    local spellCrit, rangedCrit, meleeCrit;
+    local critChance;
+
+    local result = {};
+    local holySchool = 2;
+    local minCrit = GetSpellCritChance(holySchool);
+    result.spellCrit = {};
+    result.spellCrit[holySchool] = minCrit;
+    local spellCrit;
+    for i = (holySchool + 1), MAX_SPELL_SCHOOLS do
+        spellCrit = GetSpellCritChance(i);
+        minCrit = min(minCrit, spellCrit);
+        result.spellCrit[i] = spellCrit;
+    end
+    spellCrit = minCrit
+    --远程攻击暴击几率
+    rangedCrit = GetRangedCritChance();
+    --近战暴击几率
+    meleeCrit = GetCritChance();
+
+    if (spellCrit >= rangedCrit and spellCrit >= meleeCrit) then
+        critChance = spellCrit;
+        rating = CR_CRIT_SPELL;
+    elseif (rangedCrit >= meleeCrit) then
+        critChance = rangedCrit;
+        rating = CR_CRIT_RANGED;
+    else
+        critChance = meleeCrit;
+        rating = CR_CRIT_MELEE;
+    end
+
+    local tooltip = HIGHLIGHT_FONT_COLOR_CODE ..
+        format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_CRITICAL_STRIKE) .. FONT_COLOR_CODE_CLOSE;
+    local tooltip2 = nil;
+    local extraCritChance = GetCombatRatingBonus(rating);
+    local extraCritRating = GetCombatRating(rating);
+    if (GetCritChanceProvidesParryEffect()) then
+        tooltip2 = format(CR_CRIT_PARRY_RATING_TOOLTIP, BreakUpLargeNumbers(extraCritRating), extraCritChance,
+            GetCombatRatingBonusForCombatRatingValue(CR_PARRY, extraCritRating));
+    else
+        tooltip2 = format(CR_CRIT_TOOLTIP, BreakUpLargeNumbers(extraCritRating), extraCritChance);
+    end
+
+    print(critChance, tooltip, tooltip2);
+end
+
+--获取玩家的急速信息
+function GetjiSu()
+    local haste = GetHaste();
+    local rating = CR_HASTE_MELEE;
+
+    local hasteFormatString;
+    if (haste < 0 and not GetPVPGearStatRules()) then
+        hasteFormatString = RED_FONT_COLOR_CODE .. "%s" .. FONT_COLOR_CODE_CLOSE;
+    else
+        hasteFormatString = "%s";
+    end
+
+    local tooltip = HIGHLIGHT_FONT_COLOR_CODE .. format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_HASTE) ..
+        FONT_COLOR_CODE_CLOSE;
+
+    local _, class = UnitClass(unit);
+    local tooltip2 = _G["STAT_HASTE_" .. class .. "_TOOLTIP"];
+    if (not tooltip2) then
+        tooltip2 = STAT_HASTE_TOOLTIP;
+    end
+    tooltip2 = tooltip2 ..
+        format(STAT_HASTE_BASE_TOOLTIP, BreakUpLargeNumbers(GetCombatRating(rating)), GetCombatRatingBonus(rating));
+end
+
+--精通有点特殊--12846
+function GetJitong()
+    local mastery = GetMasteryEffect();
+
+    local _, class = UnitClass("player");
+    local mastery, bonusCoeff = GetMasteryEffect();
+    local masteryBonus = GetCombatRatingBonus(CR_MASTERY) * bonusCoeff;
+
+    local primaryTalentTree = GetSpecialization();
+    if (primaryTalentTree) then
+        local masterySpell, masterySpell2 = GetSpecializationMasterySpells(primaryTalentTree);
+        local description = GetSpellDescription(masterySpell);
+        local tootiple = format(STAT_MASTERY_TOOLTIP, BreakUpLargeNumbers(GetCombatRating(CR_MASTERY)), masteryBonus);
+    else
+        local tooltip = STAT_MASTERY_TOOLTIP_NO_TALENT_SPEC;
+    end
+end
+
+--获取全能
+function GetQuanneng()
+    local versatility = GetCombatRating(CR_VERSATILITY_DAMAGE_DONE);
+    local versatilityDamageBonus = GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_DONE) +
+        GetVersatilityBonus(CR_VERSATILITY_DAMAGE_DONE);
+    local versatilityDamageTakenReduction = GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_TAKEN) +
+        GetVersatilityBonus(CR_VERSATILITY_DAMAGE_TAKEN);
+    --PaperDollFrame_SetLabelAndText(statFrame, STAT_VERSATILITY, versatilityDamageBonus, true, versatilityDamageBonus);
+    local tooltip = HIGHLIGHT_FONT_COLOR_CODE ..
+        format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_VERSATILITY) .. FONT_COLOR_CODE_CLOSE;
+    local tooltip2 = format(CR_VERSATILITY_TOOLTIP, versatilityDamageBonus, versatilityDamageTakenReduction,
+        BreakUpLargeNumbers(versatility), versatilityDamageBonus, versatilityDamageTakenReduction);
+    --statFrame:Show();
+end
+
+--获取加速
+function GetSpeed()
+    local speed = GetSpeed();
+
+    local tooltip = HIGHLIGHT_FONT_COLOR_CODE ..
+        format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_SPEED) .. " " .. format("%.2F%%", speed) .. FONT_COLOR_CODE_CLOSE;
+    local tooltip2 = format(CR_SPEED_TOOLTIP, BreakUpLargeNumbers(GetCombatRating(CR_SPEED)),
+        GetCombatRatingBonus(CR_SPEED));
+end
+
+--获取生命偷取
+
+function Getxixue()
+    local lifesteal = GetLifesteal();
+    local tooltip = HIGHLIGHT_FONT_COLOR_CODE ..
+    format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_LIFESTEAL) .. " " .. format("%.2F%%", lifesteal) .. FONT_COLOR_CODE_CLOSE;
+    local tooltip2 = format(CR_LIFESTEAL_TOOLTIP, BreakUpLargeNumbers(GetCombatRating(CR_LIFESTEAL)),
+        GetCombatRatingBonus(CR_LIFESTEAL));
+end
+
+--获取闪避
+function GetShanbi()
+    local avoidance = GetAvoidance();
+
+    local tooltip = HIGHLIGHT_FONT_COLOR_CODE ..
+    format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_AVOIDANCE) .. " " .. format("%.2F%%", avoidance) .. FONT_COLOR_CODE_CLOSE;
+    local tooltip2 = format(CR_AVOIDANCE_TOOLTIP, BreakUpLargeNumbers(GetCombatRating(CR_AVOIDANCE)),
+        GetCombatRatingBonus(CR_AVOIDANCE));
+end
