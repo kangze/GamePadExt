76a18fae1176261b806b0d42678407899cceea69
kangze <kangze25@hotmail.com>
2024-04-15 22:00:50 +0800

    update

---
+++ modules/CharacterModule/frames/templates/GpeExpansionItemTemplateMixin.lua
@@ -0,0 +1,13 @@
+GpeExpansionItemTemplateMixin = {}
+
+function GpeExpansionItemTemplateMixin:OnLoad()
+
+end
+
+function GpeExpansionItemTemplateMixin:OnEnter()
+    self.background:SetAtlas("spec-thumbnailborder-on");
+end
+
+function GpeExpansionItemTemplateMixin:OnLeave()
+
+end
---
+++ modules/CharacterModule/frames/templates/GpeFactionItemTemplateMixin.lua
@@ -0,0 +1,14 @@
+GpeFactionItemTemplateMixin = {};
+
+
+function GpeFactionItemTemplateMixin:OnLoad()
+
+end
+
+function GpeFactionItemTemplateMixin:OnEnter()
+
+end
+
+function GpeFactionItemTemplateMixin:OnLeave()
+    
+end
